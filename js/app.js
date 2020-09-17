4f0e231119475891e6330f25c20ba19ff7519dd2
Sorravit <kanomcake1928@gmail.com>
2020-09-17 16:42:17 +0700

    add image detail apge

--- js/app.js
+++ js/app.js
@@ -6,10 +6,10 @@ document.addEventListener('init', function(event) {
             document.querySelector('#myNavigator').pushPage('views/detail.html');
         };
         page.querySelector('#infinitywar').onclick = function() {
-            document.querySelector('#myNavigator').pushPage('views/detail.html');
+            document.querySelector('#myNavigator').pushPage('views/detail2.html');
         };
         page.querySelector('#endgame').onclick = function() {
-            document.querySelector('#myNavigator').pushPage('views/detail.html');
+            document.querySelector('#myNavigator').pushPage('views/detail3.html');
         };
     } else if (page.id === 'Home') {
 
