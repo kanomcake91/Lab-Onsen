609583ba465eacf8ee4659b7da584fc15a450667
Sorravit <kanomcake1928@gmail.com>
2020-09-22 00:23:30 +0700

    Add back button

--- js/app.js
+++ js/app.js
@@ -1,19 +1,31 @@
-document.addEventListener('init', function(event) {
-    var page = event.target;
+$(function() {
 
-    if (page.id === 'Home') {
-        page.querySelector('#spiderman').onclick = function() {
-            document.querySelector('#myNavigator').pushPage('views/detail.html');
-        };
-        page.querySelector('#infinitywar').onclick = function() {
-            document.querySelector('#myNavigator').pushPage('views/detail2.html');
-        };
-        page.querySelector('#endgame').onclick = function() {
-            document.querySelector('#myNavigator').pushPage('views/detail3.html');
-        };
-    } else if (page.id === 'Home') {
+    document.addEventListener('init', function(event) {
+        var page = event.target;
 
-    } else if (page.id === 'Home') {
 
-    }
-});
+        if (page.id === 'Home') {
+
+            $('#back').hide();
+            page.querySelector('#spiderman').onclick = function() {
+                document.querySelector('#myNavigator').pushPage('views/detail.html');
+
+            };
+            page.querySelector('#infinitywar').onclick = function() {
+                document.querySelector('#myNavigator').pushPage('views/detail2.html');
+            };
+            page.querySelector('#endgame').onclick = function() {
+                document.querySelector('#myNavigator').pushPage('views/detail3.html');
+            };
+        } else if (page.id === 'spiderman' || page.id === 'infinitywar' || page.id === 'endgame') {
+            $('#back').show();
+            document.querySelector('ons-back-button').onclick = function(event) {
+                document.querySelector('#myNavigator').popPage();
+            };
+
+        }
+
+
+
+    });
+})
